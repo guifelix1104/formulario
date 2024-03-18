dbc3ead5785aac3e8e17fe7212f4ed0f0adb64c1
guifelix1104 <guifelixx11@gmail.com>
2024-03-18 09:56:09 -0300

    formulario

--- scripts.js
+++ scripts.js
@@ -1,8 +1,28 @@
 var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar);
 var nome = document.getElementById("nome");
+var saida = document.getElementById("saida-de-dados");
+var email = document.getElementById("email");
+var telefone = document.getElementById("telefone");
+var cep = document.getElementById("cep");
+var lagradouro = document.getElementById("lagradouro");
+var numero = document.getElementById("numero");
+var complemento = document.getElementById("complemento");
+var bairro = document.getElementById("bairro");
+var cidade = document.getElementById("cidade");
+var estado = document.getElementById("estado");
 
-function alertar(){
 
-    alert(nome.value + "" + " clicou no botâo!!!");
+function alertar(){   
+    //alert(nome.value + "" + " clicou no botâo!!!");
+    saida.innerText = "Nome: " + "" + nome.value +
+               "\n E-mail: " + "" + email.value +
+               "\n telefone: " + "" + telefone.value +
+               "\n cep: " + "" + cep.value +
+               "\n lagradouro: " + "" + lagradouro.value +
+               "\n numero: " + "" + numero.value +
+               "\n complemento: " + "" + complemento.value +
+               "\n bairro: " + "" + bairro.value +
+               "\n cidade: " + "" + cidade.value +
+               "\n estado: " + "" + estado.value;
 }
